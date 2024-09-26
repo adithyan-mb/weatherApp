1ff8dadadfbbc61d95bb7324d55eecc534651cd9
adithyan <adithyanmbdev@gmail.com>
2024-09-26 22:07:00 +0530

    comiited

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()], // Add your required plugins here
   build: {
-    outDir: 'build', // Change this to specify a different output directory
-  },
-})
+    outDir: 'build', // The output directory for the build files
+    assetsDir: 'static', // Directory for static assets within the output directory
+    rollupOptions: {
+      output: {
+        // Customizes the output filenames
+        assetFileNames: 'static/[name].[hash][extname]', // For assets like images, fonts, etc.
+        chunkFileNames: 'static/[name].[hash].js', // For code split JS chunks
+        entryFileNames: 'static/[name].[hash].js' // For entry JS files
+      }
+    }
+  }
+});
